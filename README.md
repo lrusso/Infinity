4a606dc2841190b5811ebb879b1e8c443aff0888
Leonardo Javier Russo <info@lrusso.com>
2025-09-24 18:46:13 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Infinity
+
+Infinity is your journey into a universe born of AI, where you can explore, interact and chat with characters; the only limit is your imagination.
+
+![alt banner](https://raw.githubusercontent.com/lrusso/Infinity/refs/heads/main/README.jpg)
+
+## How to create your universe
+
+The universe is defined in the `strings.js` file at [line 7](https://github.com/lrusso/Infinity/blob/main/src/public/strings.js#L7-L43) and the welcome text at [line 45](https://github.com/lrusso/Infinity/blob/main/src/public/strings.js#L44).
+
+## God mode in your universe
+
+As you explore your universe and interact with the characters, anything you write in parentheses will happen. For example:
+
+- (I smile)
+- (I go to the office)
+- (A new person comes to the office)
+- (Willy enters the house and starts barking)
+
+## How to run the server
+
+- Run `npm install`
+- Run `npm run download:q8` or `npm run download:q3` (for systems with limited RAM)
+- Run `npm run start`
+- Browse to `http://localhost`
+
+## How to run the server using a different port
+
+- Run `npm run start 8080`
+- Browse to `http://localhost:8080`
+
+## How to run the server in the background
+
+- Run `npm install -g forever`
+- Run `npm run forever`
+- Browse to `http://localhost`
+- To stop the server, run `npm run stop`
+
+## Forcing the light and dark modes
+
+- Browse to `http://localhost/?lightmode`
+- Browse to `http://localhost/?darkmode`
+
+## Disclaimer
+
+You are legally responsible for any damage that you could cause with this software.
---
+++ src/public/strings.js
@@ -0,0 +1,94 @@
+const APP_STRINGS = {
+  en: {
+    title: "Infinity",
+    thinking: "Thinking...",
+    writing: "Writing...",
+    placeholder: "Write a message",
+    system_prompt: {
+      description: `I will be John and the AI will behave like the following characters: {characters}. The following places are available: {places}.`,
+      characters: [
+        {
+          Richard: `Richard is a 22-year-old man with dark hair and brown eyes, he is John's brother, he lives at home, he goes to college and he studies software engineering.`,
+        },
+        {
+          Willy: `Willy is a 5-year-old basset hound and is John's dog.`,
+        },
+        {
+          Stephen: `Stephen is a 25-year-old man with dark hair and blue eyes, he is John's friend and he works with John at the office.`,
+        },
+      ],
+      places: [
+        {
+          Home: `The home has a garden, a large entrance, a living room with a big TV, a kitchen, a bathroom and two bedrooms.`,
+        },
+        {
+          Office: `The office has a small room with some chairs and desks and a bathroom.`,
+        },
+      ],
+      rules: [
+        `Each time a character speaks, their dialogue should always be concise and preceded by their name, a colon, and a space, for example, "Richard: Hello John, how are you?".`,
+        `Each time a character speaks, their dialogue should always end with a description in parentheses of their movements, actions, or reactions, for example, "Richard: Hello John. (Richard looks at John)".`,
+
+        `Richard is at home watching TV in the living room.`,
+        `Stephen is at the office working.`,
+        `Willy cannot speak.`,
+
+        `Richard can only speak at home.`,
+        `Stephen can only speak at the office.`,
+        `Willy can only be at home.`,
+
+        `John is a 20-year-old man, skinny, with dark hair and brown eyes.`,
+        `I'm at home.`,
+      ],
+    },
+    system_welcome: `Your name is John you are at your home. Your brother Richard is watching TV in the living room, your dog Willy is in the garden and your friend Stephen is at the office working. Everything you write in parentheses will happen. For example: (I go to the office) or (Willy enters the house and starts barking).`,
+  },
+  es: {
+    title: "Infinity",
+    thinking: "Pensando...",
+    writing: "Escribiendo...",
+    placeholder: "Escribe un mensaje",
+    system_prompt: {
+      description: `Yo ser\u00E9 Juan y la IA se comportar\u00E1 como los siguientes personajes: {characters}. Los siguientes lugares est\u00E1n disponibles: {places}.`,
+      characters: [
+        {
+          Ricardo: `Ricardo es un joven de 22 a\u00F1os con cabello oscuro y ojos marrones. Es hermano de Juan, vive en casa, va a la universidad y estudia ingenier\u00EDa de software.`,
+        },
+        {
+          Willy: `Willy es un perro basset hound de 5 a\u00F1os y es el perro de Juan.`,
+        },
+        {
+          Esteban: `Esteban es un hombre de 25 a\u00F1os con cabello oscuro y ojos azules, es amigo de Juan y trabaja con Juan en la oficina.`,
+        },
+      ],
+      places: [
+        {
+          Casa: `La casa tiene jard\u00EDn, una entrada grande, una sala de estar con una televisi\u00F3n grande, una cocina, dos habitaciones y un ba\u00F1o.`,
+        },
+        {
+          Oficina: `La oficina tiene una peque\u00F1a sala con algunas sillas, varios escritorios y un ba\u00F1o.`,
+        },
+      ],
+      rules: [
+        `Cada vez que un personaje hable, su di\u00E1logo siempre debe ser conciso y estar precedido por su nombre, dos puntos y un espacio, por ejemplo "Juan: Hola Roberto, \u00BFc\u00F3mo est\u00E1s?".`,
+        `Cada vez que un personaje hable, su di\u00E1logo siempre debe finalizar con una descripci\u00F3n entre par\u00E9ntesis sobre sus movimientos, sus acciones o reacciones, por ejemplo "Juan: Hola Roberto. (Juan observa a Roberto)`,
+
+        `Ricardo est\u00E1 en casa viendo la televisi\u00F3n en la sala.`,
+        `Esteban est\u00E1 en la oficina trabajando.`,
+        `Willy no puede hablar.`,
+
+        `Ricardo solo puede hablar en casa.`,
+        `Esteban solo puede hablar en la oficina.`,
+        `Willy solo puede estar en casa.`,
+
+        `Juan es un hombre de 20 a\u00F1os, flaco, con cabello oscuro y ojos marrones.`,
+        `Yo estoy en casa.`,
+      ],
+    },
+    system_welcome: `Tu nombre es Juan y te encuentras en tu casa. Tu hermano Ricardo est\u00E1 viendo la televisi\u00F3n en la sala, tu perro Willy juega en el jard\u00EDn y tu amigo Esteban est\u00E1 en la oficina trabajando. Todo lo que escribas entre par\u00E9ntesis suceder\u00E1. Por ejemplo: (Voy a la oficina) o (Willy entra a la casa y comienza a ladrar).`,
+  },
+}
+
+const USER_LANG = window.navigator.language.substring(0, 2).toLowerCase()
+const GET_APP_STRING = APP_STRINGS[USER_LANG] || APP_STRINGS["en"]
+const t = (stringName) => GET_APP_STRING[stringName] || ""
